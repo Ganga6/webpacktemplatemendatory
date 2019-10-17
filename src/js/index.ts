40a7c5e8f56f089edd8bd163dbe59f92827edc32
Ganga6 <44807131+Ganga6@users.noreply.github.com>
2019-10-17 13:57:05 +0200

    add file

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,20 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+document.getElementById("doit").addEventListener("click", manipulate)
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function manipulate() {
+    let textelement: HTMLInputElement = <HTMLInputElement>document.getElementById("textin")
+    let e: HTMLSelectElement = <HTMLSelectElement>document.getElementById("select")
+    let out = document.getElementById("preview")
+    let text;
+    if (e.options[e.selectedIndex].value == "upper") {
+        text = textelement.value.toUpperCase()
+    } else {
+        text = textelement.value.toLowerCase()
+    }
+    out.textContent = text;
+    let table = <HTMLTableElement>document.getElementById("list")
+    let row = table.insertRow(0)
+    let cell1 = row.insertCell(0)
+    let cell2 = row.insertCell(1)
+    cell1.textContent = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr").length.toString()
+    cell2.textContent = text
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
